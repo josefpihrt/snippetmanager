794f6dbe6665e0e1ab70096d4182ffde3f5937e2
Josef Pihrt <josef.pihrt@outlook.com>
2016-02-20 12:18:03 +0100

    Comments added to SnippetUtility class.

--- src/SnippetManager/SnippetUtility.cs
+++ src/SnippetManager/SnippetUtility.cs
@@ -7,17 +7,23 @@ using Pihrtsoft.Snippets.Comparers;
 
 namespace Pihrtsoft.Snippets
 {
-    //TODO: CreateHtmlTable
-
+    /// <summary>
+    /// Provides a set of static methods that are related to <see cref="Snippet"/>.
+    /// </summary>
     public static class SnippetUtility
     {
+        /// <summary>
+        /// Returns enumerable groups of <see cref="Snippet"/>s that have same shortcut. <see cref="SnippetComparer.Shortcut"/> is used to compare shortcuts.
+        /// </summary>
+        /// <param name="snippets">Collection of <see cref="Snippet"/>s.</param>
+        /// <returns>Enumerable collection of <see cref="IGrouping{String, Snippet}"/> where eaech element contains shortcut and snippets with that shortcut.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="snippets"/> is <c>null</c>.</exception>
         public static IEnumerable<IGrouping<string, Snippet>> FindDuplicateShortcuts(IEnumerable<Snippet> snippets)
         {
             if (snippets == null)
                 throw new ArgumentNullException(nameof(snippets));
 
             foreach (IGrouping<string, Snippet> grouping in snippets
-                .Where(f => f.Shortcut.Length > 0)
                 .GroupBy(f => f, SnippetComparer.Shortcut))
             {
                 if (grouping.CountExceeds(1))
