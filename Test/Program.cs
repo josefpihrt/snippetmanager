9c1f900ca03e2bb492d3d6afe998fba931e1a82f
Josef Pihrt <josef.pihrt@outlook.com>
2016-02-20 13:00:37 +0100

    SnippetManagerTest project added.

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pihrtsoft.Snippets;
+
+namespace SnippetManagerTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var xx = SnippetSerializer.DeserializeFile("test.snippet");
+
+            var x = new Snippet();
+
+            x.Literals.Add(Literal.CreateClassNameLiteral("test", "t", "d"));
+
+            x.Save("test.snippet");
+
+            Console.ReadKey();
+        }
+    }
+}
