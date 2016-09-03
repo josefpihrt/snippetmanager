bc0df30fa902534b6da99c3e5eab5d6bd9a92d65
Josef Pihrt <josef.pihrt@outlook.com>
2016-09-03 16:51:27 +0200

    Change version to 1.1.0

--- src/SnippetManager/Properties/AssemblyInfo.cs
+++ src/SnippetManager/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Snippet Manager Library")]
+[assembly: AssemblyTitle("Snippet Manager")]
 [assembly: AssemblyDescription("Snippet Manager API enables to work with Visual Studio snippet format.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pihrtsoft")]
-[assembly: AssemblyProduct("Snippet Manager Library")]
+[assembly: AssemblyProduct("Snippet Manager")]
 [assembly: AssemblyCopyright("Copyright (c) 2015-2016 Josef Pihrt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: CLSCompliant(true)]
