8ec8dbcd64a1aaac3a58bf32a72938c4bd589e50
Josef Pihrt <josef.pihrt@outlook.com>
2016-09-03 16:35:43 +0200

    Add project "SnippetManagerTest"

---
+++ tools/SnippetManagerTest/Program.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Text;
+using Pihrtsoft.Snippets;
+
+namespace SnippetManagerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"..\..\Snippet.snippet";
+
+            SnippetFile snippetFile = SnippetSerializer.DeserializeFile(filePath);
+
+            var snippetCollection = snippetFile.Snippets;
+
+            if (snippetCollection.Count == 1)
+            {
+                Snippet snippet = snippetCollection[0];
+
+                var settings = new SaveSettings()
+                {
+                    IndentChars = "  ",
+                    OmitCodeSnippetsElement = true,
+                    OmitXmlDeclaration = true,
+                    Comment = " comment "
+                };
+
+                var oldValue = File.ReadAllText(filePath, Encoding.UTF8);
+                Console.WriteLine(oldValue);
+
+                Console.WriteLine();
+
+                var newValue = SnippetSerializer.CreateXml(snippet, settings);
+                Console.WriteLine(newValue);
+
+                Console.WriteLine();
+                Console.WriteLine(string.Equals(oldValue, newValue, StringComparison.Ordinal));
+            }
+
+            Console.WriteLine("*** END ***");
+            Console.ReadKey();
+        }
+    }
+}
