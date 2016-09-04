dbc0f0f74c3d3fec008c4ea136eb2dc19a073b43
Josef Pihrt <josef.pihrt@outlook.com>
2016-09-04 17:28:43 +0200

    Update SnippetUtility.FindDuplicateShortcuts method so it reflects possible alternative shortcuts

--- src/SnippetManager/SnippetUtility.cs
+++ src/SnippetManager/SnippetUtility.cs
@@ -16,18 +16,20 @@ namespace Pihrtsoft.Snippets
         /// Returns enumerable groups of <see cref="Snippet"/>s that have same shortcut. <see cref="SnippetComparer.Shortcut"/> is used to compare shortcuts.
         /// </summary>
         /// <param name="snippets">Collection of <see cref="Snippet"/>s.</param>
-        /// <returns>Enumerable collection of <see cref="IGrouping{String, Snippet}"/> where eaech element contains shortcut and snippets with that shortcut.</returns>
+        /// <returns>Enumerable collection of <see cref="DuplicateShortcutInfo"/> where each element contains shortcut and snippets with that shortcut.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="snippets"/> is <c>null</c>.</exception>
-        public static IEnumerable<IGrouping<string, Snippet>> FindDuplicateShortcuts(IEnumerable<Snippet> snippets)
+        public static IEnumerable<DuplicateShortcutInfo> FindDuplicateShortcuts(IEnumerable<Snippet> snippets)
         {
             if (snippets == null)
                 throw new ArgumentNullException(nameof(snippets));
 
-            foreach (IGrouping<string, Snippet> grouping in snippets
+            foreach (var grouping in snippets
+                .SelectMany(snippet => snippet.Shortcuts()
+                    .Select(shortcut => new { Shortcut = shortcut, Snippet = snippet }))
                 .GroupBy(f => f.Shortcut, SnippetComparer.Shortcut.GenericEqualityComparer))
             {
                 if (grouping.CountExceeds(1))
-                    yield return grouping;
+                    yield return new DuplicateShortcutInfo(grouping.Key, grouping.Select(f => f.Snippet));
             }
         }
     }
