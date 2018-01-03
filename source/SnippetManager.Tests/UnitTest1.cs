65de51d1cc529f14d6e072eeda2b5ad80cd58908
Josef Pihrt <josef.pihrt@outlook.com>
2018-01-03 20:22:58 +0100

    Add project SnippetManager.Tests

---
+++ source/SnippetManager.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
+using System.Linq;
+using System.Text;
+using Pihrtsoft.Snippets;
+using Xunit;
+
+namespace SnippetManager.Tests
+{
+    public class SnippetTests
+    {
+        [Fact]
+        public void Snippet_Deserialize()
+        {
+            const string filePath = @"..\..\..\Snippet.snippet";
+
+            Snippet snippet = null;
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                snippet = SnippetSerializer.Deserialize(stream).FirstOrDefault();
+
+            var settings = new SaveSettings()
+            {
+                IndentChars = "  ",
+                OmitCodeSnippetsElement = true,
+                OmitXmlDeclaration = true,
+                Comment = " comment "
+            };
+
+            string oldValue = File.ReadAllText(filePath, Encoding.UTF8);
+
+            string newValue = SnippetSerializer.CreateXml(snippet, settings);
+
+            Assert.Equal(oldValue, newValue);
+        }
+    }
+}
--- source/Tools/SnippetManagerTest/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Justification = "<Pending>", Scope = "member", Target = "~M:SnippetManagerTest.Program.Main(System.String[])")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "<Pending>", Scope = "member", Target = "~M:SnippetManagerTest.Program.Main(System.String[])")]
-
