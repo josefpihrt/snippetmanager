1321ff43ec38654810c163a6e8dafdd76234d133
Josef Pihrt <josef.pihrt@outlook.com>
2020-09-29 18:30:25 +0200

    Update scripts

--- tools/build.debug.cmd
+++ tools/build.debug.cmd
@@ -1,10 +1,8 @@
 @echo off
 
-dotnet restore --force "..\src\SnippetManager.sln"
-
 "C:\Program Files\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild" "..\src\SnippetManager.sln" ^
- /t:Clean,Build ^
- /p:Configuration=Release,TreatWarningsAsErrors=true,WarningsNotAsErrors=1591 ^
+ /t:Build ^
+ /p:Configuration=Debug ^
  /v:normal ^
  /fl ^
  /m
