cb64341f5d9f0a7c99899afdecafe319dd7d8e8e
Josef Pihrt <josef.pihrt@outlook.com>
2021-01-02 21:09:41 +0100

    Update scripts

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,8 +1,11 @@
 @echo off
 
+set _programFiles=%ProgramFiles(x86)%
+if not defined _programFiles set _programFiles=%ProgramFiles%
+
 dotnet restore --force "..\src\SnippetManager.sln"
 
-"C:\Program Files\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild" "..\src\SnippetManager.sln" ^
+"%_programFiles%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild" "..\src\SnippetManager.sln" ^
  /t:Clean,Build ^
  /p:Configuration=Release,TreatWarningsAsErrors=true,WarningsNotAsErrors=1591 ^
  /v:normal ^
